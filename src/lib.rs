ea6ea95d7acf90c41b472e3239f1bb25f9082486
Shrikrishna Bhat <shrikrishna.bht@gmail.com>
2023-06-30 11:23:50 +0530

    changed to print patient data

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,37 @@
+use chrono::{Datelike, Local};
+
 #[derive(Debug)]
-struct Patient {
-    name: String,
-    dob: String,
-    age: u64,
-    date: String,
+pub struct Patient {
+    pub name: String,
+    pub dob: String,
+    pub age: Option<u64>,
+    pub date: String,
 }
 
 impl Patient {
-    fn new(&self)
+    pub fn new(name: String, dob: String, age: Option<u64>, date: String) -> Patient {
+        Patient {
+            name,
+            dob,
+            age,
+            date,
+        }
+    }
+}
+
+pub fn calculate_age(dob: &str) -> Option<u64> {
+    let d_o_b = chrono::NaiveDate::parse_from_str(dob, "%Y-%m-%d").ok()?;
+
+    let current_date = Local::today().naive_local();
+    let age = current_date.year() - d_o_b.year();
+
+    let adjusted_age = if current_date.month() < d_o_b.month()
+        || (current_date.month() == d_o_b.month() && current_date.day() < d_o_b.day())
+    {
+        age - 1
+    } else {
+        age
+    };
+
+    Some(adjusted_age as u64)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,19 @@
+use mylib::*;
 
+fn main() {
+    let name: String = String::from("ABC");
+    let dob: String = String::from("1997-05-21");
+    let age: Option<u64> = calculate_age(dob.as_str());
+    let date: String = String::from("2023-06-30");
+
+    let patient = Patient::new(name, dob, age, date);
+    println!("Name: {}", patient.name);
+    println!("DOB: {}", patient.dob);
+
+    if let Some(age) = patient.age {
+        println!("Age:{}", age);
+    } else {
+        println!("Age: Unknown")
+    }
+    println!("Date: {}", patient.date);
+}
